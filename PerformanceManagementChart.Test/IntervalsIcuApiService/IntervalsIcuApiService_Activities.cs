c763265f2d04f12307fbbaf6c7afcf4b3695e4f1
Pete Fowler <fowler.pete@gmail.com>
2025-07-09 08:03:05 -0600

    Move service setup to test constructor

--- PerformanceManagementChart.Test/IntervalsIcuApiService/IntervalsIcuApiService_Activities.cs
+++ PerformanceManagementChart.Test/IntervalsIcuApiService/IntervalsIcuApiService_Activities.cs
@@ -8,13 +8,10 @@ namespace PerformanceManagementChart.Test.IntervalsIcuApiServiceTests;
 
 public class IntervalsIcuApiService_Activities
 {
-    [Theory]
-    [InlineData(
-        "athlete/12345/activities?oldest=2018-01-01&newest=2018-04-16",
-        "https://intervals.icu/api/v1/athlete/12345/activities?oldest=2018-01-01&newest=2018-04-16"
-    )]
-    [InlineData("athlete/6789/activities", "https://intervals.icu/api/v1/athlete/6789/activities")]
-    public void GetUrl_ReturnsExpected(string inputUrl, string expectedUrl)
+
+    private readonly IntervalsIcuApiService _service;
+
+    public IntervalsIcuApiService_Activities()
     {
         var mockHandler = new Mock<HttpMessageHandler>();
         mockHandler
@@ -30,9 +27,18 @@ public class IntervalsIcuApiService_Activities
         var httpClient = new HttpClient(mockHandler.Object);
         mockFactory.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(httpClient);
 
-        var service = new IntervalsIcuApiService(mockFactory.Object);
+        _service = new IntervalsIcuApiService(mockFactory.Object);
+    }
 
-        var result = service.GetUrl(inputUrl);
+    [Theory]
+    [InlineData(
+        "athlete/12345/activities?oldest=2018-01-01&newest=2018-04-16",
+        "https://intervals.icu/api/v1/athlete/12345/activities?oldest=2018-01-01&newest=2018-04-16"
+    )]
+    [InlineData("athlete/6789/activities", "https://intervals.icu/api/v1/athlete/6789/activities")]
+    public void GetUrl_ReturnsExpected(string inputUrl, string expectedUrl)
+    {
+        var result = _service.GetUrl(inputUrl);
 
         Assert.Equal(expectedUrl, result);
     }
