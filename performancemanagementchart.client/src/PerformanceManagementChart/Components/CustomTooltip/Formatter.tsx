1605fb0e6216de70a880159cca5c4a34b7c4060f
Pete Fowler <fowler.pete@gmail.com>
2025-07-28 08:19:01 -0600

    Update pace formatter

--- performancemanagementchart.client/src/PerformanceManagementChart/Components/CustomTooltip/Formatter.tsx
+++ performancemanagementchart.client/src/PerformanceManagementChart/Components/CustomTooltip/Formatter.tsx
@@ -3,15 +3,18 @@ export class Formatter {
         const hours = Math.floor(seconds / 3600);
         const minutes = Math.floor((seconds % 3600) / 60);
         const secondsLeft = Math.floor(seconds % 60);
-        return `${hours > 0 ? hours + "h" : ""}${minutes > 0 ? minutes + "m" : hours > 0 ? "0m" : ""}${
-            secondsLeft > 0 ? secondsLeft + "s" : ""
-        }`;
+        return `${hours > 0 ? hours + "h" : ""}${
+            minutes > 0 ? minutes + "m" : hours > 0 ? "0m" : ""
+        }${secondsLeft > 0 ? secondsLeft + "s" : ""}`;
     }
 
-    static formatPace(pace: number) {
-        // pace in min/mi
+    static formatPace(mph: number) {
+        if (mph <= 0) return "0:00/mi"; 
+
+        const pace = 60 / mph;
         const min = Math.floor(pace);
         const sec = Math.round((pace - min) * 60);
+        
         return `${min}:${sec.toString().padStart(2, "0")}/mi`;
     }
 
